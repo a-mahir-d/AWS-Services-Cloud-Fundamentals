1478729a47b01c081c37d0dcf027cd7f45028419
a-mahir-d <amd.coding.ac@gmail.com>
2026-05-02 21:37:14 +0300

    first commit

---
+++ SQSConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var region = Amazon.RegionEndpoint.EUNorth1;
+var sqsClient = new AmazonSQSClient(region);
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+};
+
+var cts = new CancellationTokenSource();
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+    if (response?.Messages?.Count > 0)
+    {
+        foreach (var message in response.Messages)
+        {
+            Console.WriteLine($"Message Id: {message.MessageId}, Body: {message.Body}");
+            await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+        }
+    }
+
+    await Task.Delay(1000, cts.Token);
+}
---
+++ SQSPublisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var accessKey = "";
+var secretKey = "";
+var region = Amazon.RegionEndpoint.EUNorth1;
+
+// var sqsClient = new AmazonSQSClient(accessKey, secretKey, region);  // Console'daki kullanıcı verilerine erişim sağlayabiliyor
+var sqsClient = new AmazonSQSClient(region);
+
+var customer = new
+{
+    FirstName = "John",
+    LastName = "Doe",
+    Age = 35
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = "Customer"
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine("Message sent");
